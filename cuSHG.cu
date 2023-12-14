25e8b141ef8f2aaf4b2a1b35892de3623ea4c6e5
Alfredo Daniel Sanchez <45202555+alfredos84@users.noreply.github.com>
2023-12-14 19:23:34 +0100

    Update cuSHG.cu

--- cuSHG.cu
+++ cuSHG.cu
@@ -83,6 +83,7 @@ int main(int argc, char *argv[]){
 	
 	////////////////////////////////////////////////////////////////////////////////////////
 	// 4. Reset the GPU and finish simulation timing
+
 	std::cout << "\n\nDevice reset" << std::endl;
 	cudaDeviceReset();		
 	
